e64ee88a96d16f85e6311c127fbab26a976d99ba
shecannotsee <shecannotsee@163.com>
2023-05-24 18:49:32 +0800

    pretty

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,20 @@
 </template>
 
 <style>
-html, body, #app {
-  background-color: #555;
-  margin-bottom: 0;
+// home and login button design
+.button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh; /* 可根据需要调整高度 */
+}
+*{
+  padding: 0px;
+  margin: 0px;
+}
+html,body,#app{
+  width: 100%;
+  height: 100%;
 }
 </style>
 
