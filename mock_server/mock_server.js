8788a3a0353b3de860343401b66a157bc2f142f1
shecannotsee <shecannotsee@163.com>
2023-05-25 14:26:51 +0800

    [mock]添加mock server,目前该mock server只会处理一次请求，然后就报错

--- mock_server/mock_server.js
+++ mock_server/mock_server.js
@@ -1,38 +1,60 @@
+const cors = require('cors');
 const http = require('http');
+const fs = require('fs');
+
+function readJsonFile(filename) {
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    const obj = JSON.parse(data);
+    return obj;
+  } catch (err) {
+    throw err;
+  }
+};
 
 // 创建服务器
 const server = http.createServer((req, res) => {
-  // 设置响应头
-  res.setHeader('Content-Type', 'application/json');
+  var once = 0;
+  if (once == 0) {
+    // 设置响应头，允许来自任意源的跨域请求
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // 设置响应头
+    res.setHeader('Content-Type', 'application/json');
+    once = 1;
+  }
+
+  // 读取mock数据,url是路径,data是数据
+  const user   = readJsonFile("./data/user.json");
+  const people = readJsonFile("./data/people.json");
+
 
   // 根据请求路径返回不同的 mock 数据
-  /*z*/if (req.url === '/api/users') {
-    const users = [
-      { id: 1, name: 'John' },
-      { id: 2, name: 'Jane' },
-      { id: 3, name: 'Bob' }
-    ];
+  /*z*/if (req.url === user.url) {
+    console.log("in:",user.url);
+    const ret = user.data;
     res.statusCode = 200;
-    res.end(JSON.stringify(users));
+    res.end(JSON.stringify(ret));
   } 
-  else if (req.url === '/api/posts') {
-    const posts = [
-      { id: 1, title: 'Post 1' },
-      { id: 2, title: 'Post 2' },
-      { id: 3, title: 'Post 3' }
-    ];
+  else if (req.url === people.url) {
+    console.log("in:",people.url);
+    const ret = people.data;
     res.statusCode = 200;
-    res.end(JSON.stringify(posts));
+    res.end(JSON.stringify(ret));
   } 
   else {
     res.statusCode = 404;
-    res.end(JSON.stringify({ message: 'Not found' }));
+    res.end(JSON.stringify({ message: 'mock server:Not found' }));
   }
 });
 
+// 使用 cors 中间件
+server.on('request', cors());
+
 // 启动服务器并监听指定端口
-const port = 5173;
+const port = 9703;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
