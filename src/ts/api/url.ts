85efcd7d0302bee0323a6e9c3a71a0279540ab2c
shecannotsee <shecannotsee@163.com>
2023-05-25 14:28:04 +0800

    [mock]修改向mock server的请求端口，非前端项目所绑定的端口

--- src/ts/api/url.ts
+++ src/ts/api/url.ts
@@ -1,6 +1,6 @@
 const baseUrl = () => {
   const host = window.location.hostname;
-  const port = window.location.port;
+  const port = "9703";
   const protocol = window.location.protocol;
   return protocol + "//" + host + ":" + port;
 };
