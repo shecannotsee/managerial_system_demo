4b34f87254a1b1ca0d08e9cc47e9847aac7c3a10
shecannotsee <shecannotsee@163.com>
2023-05-29 18:35:53 +0800

    [feat]添加日历

--- src/ts/timeAndDate.ts
+++ src/ts/timeAndDate.ts
@@ -1,8 +1,17 @@
 console.log("step to timeAndDate.vue")
-export default {
+import { defineComponent, ref } from 'vue';
+import { Dayjs } from 'dayjs';
+
+export default defineComponent({
   setup() {
+    const value = ref<Dayjs>();
+    const onPanelChange = (value: Dayjs, mode: string) => {
+      console.log(value, mode);
+    };
 
-
-    return {}
-  }
-}
+    return {
+      value,
+      onPanelChange,
+    };
+  },
+});
--- src/view/timeAndDate.vue
+++ src/view/timeAndDate.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <p>time and date</p>
+  <div :style="{ width: '300px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
+    <a-calendar v-model:value="value" :fullscreen="false" @panelChange="onPanelChange" />
   </div>
 </template>
 
