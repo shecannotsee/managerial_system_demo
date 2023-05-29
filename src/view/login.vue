7fd1b0701e4aa2964fc1baa065353830749021ec
shecannotsee <shecannotsee@163.com>
2023-05-29 10:52:50 +0800

    [docs]添加注释

--- src/view/login.vue
+++ src/view/login.vue
@@ -16,6 +16,7 @@
     @finish="onFinish"
     @finishFailed="onFinishFailed"
   >
+    <!-- 账号 -->
     <a-form-item
       label="账号"
       name="username"
@@ -25,6 +26,7 @@
       <a-input v-model:value="formState.username" style="width: 500px"/>
     </a-form-item>
 
+    <!-- 密码 -->
     <a-form-item
       label="密码"
       name="password"
@@ -34,10 +36,12 @@
       <a-input-password v-model:value="formState.password" style="width: 500px"/>
     </a-form-item>
 
+    <!-- 记住账号和密码 -->
     <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
       <a-checkbox v-model:checked="formState.remember">记住账号和密码</a-checkbox>
     </a-form-item>
 
+    <!-- 提交按钮 -->
     <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
       <a-button type="primary" html-type="submit">提交</a-button>
     </a-form-item>
