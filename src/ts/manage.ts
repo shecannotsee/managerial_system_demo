5ef2dc8f0aded2f6bc88c79c87c46599f3179c4b
shecannotsee <shecannotsee@163.com>
2023-05-29 17:46:08 +0800

    [feat]去除注释的代码

--- src/ts/manage.ts
+++ src/ts/manage.ts
@@ -1,36 +1,3 @@
-// import { defineComponent, reactive, toRefs } from 'vue';
-// import {
-//   HomeOutlined,
-//   CalendarOutlined,
-//   AppstoreOutlined,
-//   SettingOutlined,
-// } from '@ant-design/icons-vue';
-// import type { MenuTheme } from 'ant-design-vue';
-// export default defineComponent({
-//   components: {
-//     HomeOutlined,
-//     CalendarOutlined,
-//     AppstoreOutlined,
-//     SettingOutlined,
-//   },
-//   setup() {
-//     const state = reactive({
-//       theme: 'dark' as MenuTheme,
-//       selectedKeys: ['1'],
-//       openKeys: ['query'],
-//     });
-//     const changeTheme = (checked: boolean) => {
-//       state.theme = checked ? 'dark' : 'light';
-//     };
-//
-//     return {
-//       ...toRefs(state),
-//       changeTheme,
-//     };
-//   },
-// });
-//
-
 import {
   HomeOutlined,
   CalendarOutlined,
