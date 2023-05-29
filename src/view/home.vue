5f303ba5ed3bad574cf126ef16dff4eafbc36aca
shecannotsee <shecannotsee@163.com>
2023-05-29 10:46:11 +0800

    [feat]调整首页的样式

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Navigation buttons -->
   <div class="button-container">
     <router-link to="/home">
       <a-button>首页</a-button>
@@ -8,18 +9,28 @@
     </router-link>
   </div>
 
-  <div>首页</div>
-      
+  <!-- Main content -->
+  <div class="home-container">
+    <h1>Welcome to the Home Page</h1>
+    <p>Modify this section to showcase your content.</p>
+  </div>
 </template>
-  
+
 <style>
 .button-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 10vh; /* 可根据需要调整高度 */
+  height: 10vh; /* You can adjust the height as needed */
+}
+
+.home-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 20vh; /* Adjust the height to fill the remaining space */
 }
 </style>
-  
+
 <script lang="ts" src="../ts/home.ts"></script>
-  
