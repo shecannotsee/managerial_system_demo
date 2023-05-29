28ae3eb80f8e403acbbcfb75679b56aaa3fe4192
shecannotsee <shecannotsee@163.com>
2023-05-29 10:41:44 +0800

    [feat]调整登录页面样式

--- src/view/login.vue
+++ src/view/login.vue
@@ -20,16 +20,18 @@
       label="账号"
       name="username"
       :rules="[{ required: true, message: 'Please input your username!' }]"
+      class="center-input"
     >
-      <a-input v-model:value="formState.username" style="width: 300px"/>
+      <a-input v-model:value="formState.username" style="width: 500px"/>
     </a-form-item>
 
     <a-form-item
       label="密码"
       name="password"
       :rules="[{ required: true, message: 'Please input your password!' }]"
+      class="center-input"
     >
-      <a-input-password v-model:value="formState.password" style="width: 300px"/>
+      <a-input-password v-model:value="formState.password" style="width: 500px"/>
     </a-form-item>
 
     <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
@@ -49,6 +51,11 @@
   justify-content: center;
   align-items: center;
   height: 10vh; /* 可根据需要调整高度 */
+}
+
+.center-input {
+  display: flex;
+  justify-content: center;
 }
 </style>
 
