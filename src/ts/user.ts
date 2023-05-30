cf72cfc80c3d563d784ac3d0bd8ec2af0ca9ea85
shecannotsee <shecannotsee@163.com>
2023-05-30 11:38:01 +0800

    [fix]修改返回类型

--- src/ts/user.ts
+++ src/ts/user.ts
@@ -1,33 +1,32 @@
 import { defineComponent } from 'vue';
-import { ref } from 'vue';
 import type { user } from "@/ts/api/getUser.ts"
 import { getUser } from "@/ts/api/getUser.ts"
 
 // 表格设置
 const columns = [
-    {
-        title: '姓名',
-        dataIndex: 'name',
-    },
-    {
-        title: '年龄',
-        dataIndex: 'age',
-    },
-    {
-        title: '地址',
-        dataIndex: 'address',
-    },
-    {
-        title: '标签',
-        dataIndex: 'tags',
-    },
+  {
+    title: '姓名',
+    dataIndex: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+  },
+  {
+    title: '地址',
+    dataIndex: 'address',
+  },
+  {
+    title: '标签',
+    dataIndex: 'tags',
+  },
 ];
 
 // 通过接口获取真实数据
-let data : user = await getUser();
+let data : user[] = await getUser();
 
 async function updateData() {
-  data  = await getUser();
+  data = await getUser();
 }
 
 export default defineComponent({
