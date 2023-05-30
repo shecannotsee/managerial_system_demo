84afd6a81f92e1a48b81803d17455d25c8a47d6c
shecannotsee <shecannotsee@163.com>
2023-05-30 14:24:09 +0800

    [feat]添加按钮查询实时更新数据的功能

--- src/view/user.vue
+++ src/view/user.vue
@@ -1,18 +1,48 @@
 <template>
-  <a-table :columns="columns" :data-source="data" bordered>
+  <a-table :columns="columns" :data-source="pageData.dataList" bordered>
     <template #bodyCell="{ column, text }">
       <template v-if="column.dataIndex === 'name'">
         <a>{{ text }}</a>
       </template>
     </template>
     <template #title>
-      <a-button type="primary" v-on:click="refreshData">刷新</a-button>
+      <a-button type="primary" v-on:click="refreshData">查询</a-button>
       这里是开头</template>
     <template #footer>再往下没有了！</template>
   </a-table>
 </template>
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import type { user } from "@/ts/api/getUser.ts"
+import { getUser } from "@/ts/api/getUser.ts"
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+  },
+  {
+    title: '地址',
+    dataIndex: 'address',
+  },
+  {
+    title: '标签',
+    dataIndex: 'tags',
+  },
+];
+let pageData = reactive<{dataList:Array<user>}>({
+  dataList: []
+})
 
-<style>
-</style>
+const getUserList = async () => {
+  let { user } = await getUser()
+  pageData.dataList = user
+}
 
-<script lang="ts" src="../ts/user.ts"></script>
+const refreshData = () => {
+  getUserList()
+}
+</script>
