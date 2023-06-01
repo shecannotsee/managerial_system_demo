40ad7a370089b0088aa8fad736991b3bcfda6472
shecannotsee <shecannotsee@163.com>
2023-06-01 15:06:47 +0800

    [fix]修改接口暴露问题

--- src/view/projectByMonth.vue
+++ src/view/projectByMonth.vue
@@ -8,7 +8,7 @@
       <template #title>
         <a-input v-model:value="inputYear" placeholder="年:20xx" style="width: 100px"/>
         <a-input v-model:value="inputMonth" placeholder="月:1-12" style="width: 100px"/>
-        <a-button type="primary" v-on:click="getProjectByMonth">查询</a-button><br/><br/>
+        <a-button type="primary" v-on:click="refreshData">查询</a-button><br/><br/>
       </template>
       <template #footer>再往下没有了！</template>
     </a-table>
